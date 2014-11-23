1f1c5cb73fe51fa457b7e831e864430bb92a95dc
Gerrit <Gerrit@CuraMiPcLap1>
2014-11-23 15:21:08 +0100

    restart

--- coWiki/src/com/scimsoft/cowiki/helpers/JSONParser.java
+++ coWiki/src/com/scimsoft/cowiki/helpers/JSONParser.java
@@ -11,12 +11,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 
