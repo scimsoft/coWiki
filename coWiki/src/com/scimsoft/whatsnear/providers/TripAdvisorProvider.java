2764f1d7fde3be3e038df5c8bb6124468f7209d4
Gerrit <Gerrit@CuraMiPcLap1>
2015-01-14 09:50:27 +0100

    tracker

---
+++ coWiki/src/com/scimsoft/whatsnear/providers/TripAdvisorProvider.java
@@ -0,0 +1,97 @@
+package com.scimsoft.whatsnear.providers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.scimsoft.whatsnear.MainActivity;
+import com.scimsoft.whatsnear.helpers.Coordinates;
+import com.scimsoft.whatsnear.helpers.JSONParser;
+
+public class TripAdvisorProvider extends Providers {
+
+	public TripAdvisorProvider(MainActivity mainActivity) {
+		super(mainActivity);
+	}
+
+	JSONParser parser = new JSONParser();
+
+	private JSONObject tripResponse;
+	private String titles;
+
+	private JSONArray query;
+
+	public String getDetailExtract(String title) {
+		List<NameValuePair> params = new ArrayList<NameValuePair>(5);
+		params.add(new BasicNameValuePair("key", "ce0438de86eb46bf80da67f774ae36b3"));
+
+		try {
+			String country = mainActivity.localeProvider.getLocale().getLanguage();
+			String wikiUrl = "http://api.tripadvisor.com/api/partner/2.0/map/";
+
+			tripResponse = parser.makeHttpRequest(wikiUrl, params);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String text = "";
+		try {
+			JSONObject query = tripResponse.getJSONObject("query");
+			JSONObject pages = query.getJSONObject("pages");
+			@SuppressWarnings("unchecked")
+			Iterator<String> iterator = pages.keys();
+			String key = "";
+			while (iterator.hasNext()) {
+				key = iterator.next();
+				text = (String) ((JSONObject) pages.get(key)).get("extract");
+			}
+
+		} catch (JSONException e) {
+
+			e.printStackTrace();
+		}
+
+		return text;
+	}
+
+	public List<String> getNearbyWikiEntries(Coordinates coordinates) {
+		List<NameValuePair> params = new ArrayList<NameValuePair>(5);
+		params.add(new BasicNameValuePair("key", "ce0438de86eb46bf80da67f774ae36b3"));
+
+		try {
+			String wikiUrl = "http://api.tripadvisor.com/api/partner/2.0/map/";
+			wikiUrl += coordinates.getLatitude() + "," + coordinates.getLongtitude();
+			tripResponse = parser.makeHttpRequest(wikiUrl, params);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			query = tripResponse.getJSONArray("data");
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<String> titleList = new ArrayList<String>();
+		for (int i = 0; i < query.length(); i++) {
+			try {
+				titleList.add(query.getJSONObject(i).getString("name"));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return titleList;
+	}
+
+}
